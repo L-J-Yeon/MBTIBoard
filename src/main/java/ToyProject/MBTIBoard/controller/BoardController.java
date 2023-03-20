c21190427a8ef50d670f9681e60fb11aee3a6084
L-J-Yeon <honeyb0201@gmail.com>
2023-03-20 14:16:19 +0900

    BoardList Page 처리 완료

--- src/main/java/ToyProject/MBTIBoard/controller/BoardController.java
+++ src/main/java/ToyProject/MBTIBoard/controller/BoardController.java
@@ -5,6 +5,10 @@ import ToyProject.MBTIBoard.dto.BoardDto;
 import ToyProject.MBTIBoard.service.BoardService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -45,14 +49,6 @@ public class BoardController {
         return "redirect:/";
     }
 
-    @GetMapping(value = "/list")
-    public String boardList(Model model) {
-        List<Board> boards = boardService.findAllBoard();
-        model.addAttribute("boards", boards);
-
-        return "board/boardList";
-    }
-
     @GetMapping(value = "/detail/{id}")
     public String boardDetail(@PathVariable("id") Long id, Model model) {
         model.addAttribute("board", boardService.boardDetail(id));
@@ -80,5 +76,19 @@ public class BoardController {
         boardService.saveBoard(boardTemp);
 
         return "redirect:/board/list";
+    }
+
+    @GetMapping(value = "/list")
+    public String boardList(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC)Pageable pageable) {
+        Page<Board> list = boardService.boardList(pageable);
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage -4, 1);
+        int endPage = Math.min(nowPage + 9, list.getTotalPages());
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "board/boardList";
     }
 }
--- src/main/java/ToyProject/MBTIBoard/service/BoardService.java
+++ src/main/java/ToyProject/MBTIBoard/service/BoardService.java
@@ -3,6 +3,8 @@ package ToyProject.MBTIBoard.service;
 import ToyProject.MBTIBoard.domain.Board;
 import ToyProject.MBTIBoard.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,15 +20,15 @@ public class BoardService {
         return boardRepository.save(board);
     }
 
-    public List<Board> findAllBoard() {
-        return boardRepository.findAll();
-    }
-
     public Board boardDetail(Long id) {
         return boardRepository.findById(id).get();
     }
 
     public void boardDelete(Long id) {
         boardRepository.deleteById(id);
+    }
+
+    public Page<Board> boardList(Pageable pageable) {
+        return boardRepository.findAll(pageable);
     }
 }
